e474fa3a071d208ff91323e7e88406d8c9434ff8
Giacomo Gregoletto <greguz@users.noreply.github.com>
2020-06-02 11:07:47 +0200

    Bind error handler to instance (v3) (#2306)
    * Bind error handler to instance
    
    * Update error handler binding tests
    
    * Update setErrorHandler docs

--- test/request-error.test.js
+++ test/request-error.test.js
@@ -73,3 +73,79 @@ test('default 400 on request error with custom error handler', t => {
     })
   })
 })
+
+test('error handler binding', async t => {
+  t.plan(4)
+
+  const fastify = Fastify()
+
+  fastify.setErrorHandler(function (err, request, reply) {
+    t.strictEqual(this, fastify)
+    reply
+      .code(err.statusCode)
+      .type('application/json; charset=utf-8')
+      .send(err)
+  })
+
+  fastify.post('/', function (req, reply) {
+    reply.send({ hello: 'world' })
+  })
+
+  const res = await fastify.inject({
+    method: 'POST',
+    url: '/',
+    simulate: {
+      error: true
+    },
+    body: {
+      text: '12345678901234567890123456789012345678901234567890'
+    }
+  })
+  t.strictEqual(res.statusCode, 400)
+  t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.deepEqual(res.json(), {
+    error: 'Bad Request',
+    message: 'Simulated',
+    statusCode: 400
+  })
+})
+
+test('encapsulated error handler binding', async t => {
+  t.plan(5)
+
+  const fastify = Fastify()
+  fastify.decorate('test', 'root')
+
+  fastify.register(async function (app) {
+    app.decorate('test', 'plugin')
+    app.post('/', function (req, reply) {
+      reply.send({ hello: 'world' })
+    })
+    app.setErrorHandler(async function (err, request, reply) {
+      t.strictEqual(this.test, 'plugin')
+      reply
+        .code(err.statusCode)
+        .type('application/json; charset=utf-8')
+        .send(err)
+    })
+  })
+
+  const res = await fastify.inject({
+    method: 'POST',
+    url: '/',
+    simulate: {
+      error: true
+    },
+    body: {
+      text: '12345678901234567890123456789012345678901234567890'
+    }
+  })
+  t.strictEqual(res.statusCode, 400)
+  t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.deepEqual(res.json(), {
+    error: 'Bad Request',
+    message: 'Simulated',
+    statusCode: 400
+  })
+  t.strictEqual(fastify.test, 'root')
+})
